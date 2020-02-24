d3fa1c946415ade64b1a631a5e0964b12718c2d3
Randy <randyisbusy@gmail.com>
2020-02-24 14:16:25 -0700

    will update to use url provided in add toy form for dispaly as new toy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-      addToysToDiv(); 
-      const submit = document.getElementsByClassName("submit"); 
-      submit[0].addEventListener("click", e => { 
-          e.preventDefault();  
-          postToy() }
-          );
+      addToysToDiv();  
+      const toyButton = document.getElementsByClassName("add-toy-form");
+      
+     
+      toyButton[0].addEventListener("submit", e => { e.preventDefault(), postToy(e)}); 
+
+      //submit[0].addEventListener("submit", e => e.preventDefault(),  
+       //   postToy() 
+     //     );
 
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
@@ -92,11 +95,52 @@ function updatePatch(e){
         
 } 
 
-function postToy(){  
-  
-  formData = new FormData
-  
-  
-  debugger
+function postToy(e){  
+  const toyForm = document.getElementsByClassName("add-toy-form"); 
+  let name = e.target.name.value 
+  let image = e.target.image.value 
+  const url = 'http://localhost:3000/toys';
+  const configPost = {  
+        method: "POST",
+        headers:
+        {
+         "Content-Type": "application/json",
+         Accept: "application/json"
+         },
+
+       body: JSON.stringify({
+        "name": name ,
+        "image": image,
+        "likes": 0
+         })
+
+      }
+      fetch(url, configPost).then(function(response){ return response.json() })
+      .then( jsonData => {  
+
+        const toyBox = document.getElementById("toy-collection"); 
+        divEl = document.createElement("div");
+        divEl.setAttribute("class", "card");
+        divEl.setAttribute("id", jsonData.id );
+        hEl = document.createElement("h2");
+        hEl.innerHTML = jsonData.name
+        imgEl = document.createElement("img"); 
+        
+        imgEl.setAttribute("src", jsonData.image );
+        imgEl.setAttribute("class", "toy-avatar");
+        pEl = document.createElement("p");
+        pEl.innerHTML = jsonData.likes;
+        buttonEl = document.createElement("button");
+        buttonEl.innerHTML = "like";
+        buttonEl.setAttribute("class", "like-btn");
+        buttonEl.addEventListener("click", updatePatch);
+        divEl.appendChild(pEl)
+        divEl.appendChild(buttonEl)
+        divEl.appendChild(hEl)
+        divEl.appendChild(imgEl)
+        toyBox.appendChild(divEl)
+
+      } )
+
 
 }
